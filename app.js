3209f19707327baedc8fcb649b7c6f45705cebba
flenjorr <flenjorr@gmail.com>
2023-06-29 18:44:19 +0100

    TicTacToe

--- app.js
+++ app.js
@@ -4,11 +4,22 @@
 const player1Btn = document.getElementById('edit-player1-btn');
 const player2Btn = document.getElementById('edit-player2-btn');
 
+// opening and closing overlay
 const playerConfigOverlay = document.getElementById('config-overlay');
 const backdrop = document.getElementById('backdrop');
 const cancel = document.getElementById('cancel');
 
+// form submission -- updtaing player name
+
+const formElement = document.querySelector('form');
+const outputError = document.getElementById('output-error');
+
+
+
+
+// click events
 player1Btn.addEventListener('click', openPlayer1Edit);
 player2Btn.addEventListener('click', openPlayer1Edit);
 cancel.addEventListener('click', closePlayerConfig);
 backdrop.addEventListener('click', closePlayerConfig);
+formElement.addEventListener('submit', savePlayerConfig);
--- config.js
+++ config.js
@@ -11,3 +11,18 @@ function closePlayerConfig() {
     playerConfigOverlay.style.display = 'none';
     backdrop.style.display = 'none';
 }
+
+// form submission
+ function savePlayerConfig(event) {
+     event.preventDefault(); // prevent browser from carrying out default function on for submission
+     const formData = new FormData(event.target);
+     const enteredPlayerName = formData.get('playername').trim(); //trims extra whitespace
+
+    // validating user input
+    if(!enteredPlayerName) {
+        alert('Please enter a valid name');
+        // outputError.textContent = 'please enter a valid name!'
+        return;
+    } 
+
+}
